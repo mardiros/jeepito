0e9081910c68c7c21f93aea8f0af1b6796b3c4cc
Guillaume Gauvrit <guillaume@gauvr.it>
2023-06-20 23:46:57 +0200

    Expose classes on base module

--- src/messagebus/__init__.py
+++ src/messagebus/__init__.py
@@ -1,3 +1,30 @@
+"""
+messagebus is a library to send message in a bus in order to get base classes
+for event driven development and domain driven development.
+
+"""
 from importlib.metadata import version
+from .domain.model import Command, Event, Message, Metadata, Model
+from .service._async.registry import AsyncMessageRegistry
+from .service._async.repository import AsyncAbstractRepository
+from .service._async.unit_of_work import AsyncAbstractUnitOfWork
+
+from .service._sync.registry import SyncMessageRegistry
+from .service._sync.repository import SyncAbstractRepository
+from .service._sync.unit_of_work import SyncAbstractUnitOfWork
 
 __version__ = version("messagebus")
+
+__all__ = [
+    "Command",
+    "Event",
+    "Message",
+    "Metadata",
+    "Model",
+    "AsyncMessageRegistry",
+    "AsyncAbstractRepository",
+    "AsyncAbstractUnitOfWork",
+    "SyncMessageRegistry",
+    "SyncAbstractRepository",
+    "SyncAbstractUnitOfWork",
+]
